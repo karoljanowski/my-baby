5832970d2ce061384df53d243a1a63c99c670544
karoljanowski <karol.jj@icloud.com>
2025-10-05 19:16:03 +0200

    fix db

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
