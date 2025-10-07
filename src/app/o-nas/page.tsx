666bf84ff9e250b942f81cdb87a34f9078fbdec3
karoljanowski <karol.jj@icloud.com>
2025-10-07 23:05:04 +0200

    add about page

--- src/app/o-nas/page.tsx
+++ src/app/o-nas/page.tsx
@@ -1,5 +1,17 @@
+import AboutTop from "@/components/About/AboutTop/AboutTop";
+import DiscoverDiary from "@/components/About/DiscoverDiary/DiscoverDiary";
+import StartDiary from "@/components/About/StartDiary/StartDiary";
+import MeetDiary from "@/components/About/MeetDiary/MeetDiary";
+
 const AboutPage = () => {
-    return <div>AboutPage</div>;
+    return (
+        <div>
+            <AboutTop />
+            <MeetDiary />
+            <StartDiary />
+            <DiscoverDiary />
+        </div>
+    );
 };
 
 export default AboutPage;   
---
+++ src/components/About/AboutTop/AboutTop.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@/components/ui/badge";
+import BorderImage from "@/components/BorderImage";
+
+const AboutTop = () => {
+    return (
+        <div className="bg-light-grey py-16">
+            <div className="container mx-auto px-4">
+                <Badge className="mb-4">O nas</Badge>
+                <h1 className="text-[32px] md:text-[40px] leading-[120%] mb-8 text-dark -tracking-[0.06em]">Dlaczego nasz <span className="text-secondary font-secondary">dziennik</span> jest tak wyjątkowy?</h1>
+                <p className="text-base leading-[150%] font-light mb-26 max-w-[490px]">Zachowaj najpiękniejsze wspomnienia – zapisuj ważne chwile i dodawaj zdjęcia, by stworzyć wyjątkową pamiątkę na lata.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutTop;
