d2b282a155d88ef1d91dd0f7ba9a346ce05fcb6f
karoljanowski <karol.jj@icloud.com>
2025-10-08 20:49:23 +0200

    fix headerInfo

--- src/components/Header/HeaderInfo.tsx
+++ src/components/Header/HeaderInfo.tsx
@@ -1,7 +1,7 @@
 const HeaderInfo = () => {
     return (
-        <div className="bg-secondary text-nowrap text-secondary-foreground text-center px-2 py-2 text-sm overflow-hidden">
-            <span className="animate-scroll-text block"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </span>
+        <div className="bg-secondary text-nowrap text-secondary-foreground text-center px-2 py-2 text-sm overflow-hidden overflow-x-auto scrollbar-hide">
+            <span className="block"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </span>
         </div>
     );
 };
